912feaae5df35a3261cdb3a564386eb9a8807fe3
soir20 <71418127+soir20@users.noreply.github.com>
2020-12-28 13:27:20 -0500

    Update atlas location ref to mod class

--- src/main/java/io/github/soir20/moremcmeta/client/ClientEventSubscriber.java
+++ src/main/java/io/github/soir20/moremcmeta/client/ClientEventSubscriber.java
@@ -1,7 +1,6 @@
 package io.github.soir20.moremcmeta.client;
 
 import io.github.soir20.moremcmeta.MoreMcmeta;
-import net.minecraft.inventory.container.PlayerContainer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,7 +12,7 @@ public final class ClientEventSubscriber {
 
     @SubscribeEvent
     public static void onTextureStitchEvent(TextureStitchEvent.Pre event) {
-        if (event.getMap().getTextureLocation() == PlayerContainer.LOCATION_BLOCKS_TEXTURE) {
+        if (event.getMap().getTextureLocation() == MoreMcmeta.ATLAS_LOCATION) {
             event.addSprite(new ResourceLocation("minecraft:entity/bat"));
         }
     }
