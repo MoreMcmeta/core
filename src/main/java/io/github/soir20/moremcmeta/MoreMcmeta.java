b28eb31be2ec2eac6de0d5a7a6629d6116cb38a6
soir20 <71418127+soir20@users.noreply.github.com>
2021-01-11 19:10:16 -0500

    Add resource listener in event for thread safety

--- src/main/java/io/github/soir20/moremcmeta/MoreMcmeta.java
+++ src/main/java/io/github/soir20/moremcmeta/MoreMcmeta.java
@@ -1,13 +1,5 @@
 package io.github.soir20.moremcmeta;
 
-import io.github.soir20.moremcmeta.client.renderer.texture.AnimatedTexture;
-import io.github.soir20.moremcmeta.client.renderer.texture.TextureManagerWrapper;
-import io.github.soir20.moremcmeta.resource.AtlasReloadListener;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.data.AnimationMetadataSection;
-import net.minecraft.resources.IReloadableResourceManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(MoreMcmeta.MODID)
@@ -15,16 +7,4 @@ public final class MoreMcmeta
 {
     public static final String MODID = "moremcmeta";
     public static final String[] FOLDERS = {"entity", "gui", "map"};
-
-    public MoreMcmeta() {
-        Minecraft minecraft = Minecraft.getInstance();
-        IReloadableResourceManager manager =
-                ((IReloadableResourceManager) minecraft.getResourceManager());
-        TextureManagerWrapper texManager = new TextureManagerWrapper(minecraft.getTextureManager());
-        Logger logger = LogManager.getLogger();
-
-        manager.addReloadListener(new AtlasReloadListener<>(FOLDERS, texManager,
-                AnimatedTexture::new, AnimationMetadataSection.SERIALIZER, logger));
-        logger.debug("Added atlas reload listener");
-    }
 }
---
+++ src/main/java/io/github/soir20/moremcmeta/client/ClientModEventSubscriber.java
@@ -0,0 +1,33 @@
+package io.github.soir20.moremcmeta.client;
+
+import io.github.soir20.moremcmeta.MoreMcmeta;
+import io.github.soir20.moremcmeta.client.renderer.texture.AnimatedTexture;
+import io.github.soir20.moremcmeta.client.renderer.texture.TextureManagerWrapper;
+import io.github.soir20.moremcmeta.resource.AtlasReloadListener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.data.AnimationMetadataSection;
+import net.minecraft.resources.IReloadableResourceManager;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod.EventBusSubscriber(modid = MoreMcmeta.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientModEventSubscriber {
+
+    @SubscribeEvent
+    public static void onPreInit(final FMLConstructModEvent event) {
+        Minecraft minecraft = Minecraft.getInstance();
+        IReloadableResourceManager manager =
+                ((IReloadableResourceManager) minecraft.getResourceManager());
+        TextureManagerWrapper texManager = new TextureManagerWrapper(minecraft.getTextureManager());
+        Logger logger = LogManager.getLogger();
+
+        manager.addReloadListener(new AtlasReloadListener<>(MoreMcmeta.FOLDERS, texManager,
+                AnimatedTexture::new, AnimationMetadataSection.SERIALIZER, logger));
+        logger.debug("Added atlas reload listener");
+    }
+
+}
