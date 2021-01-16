ded26943008074d1ab1ad527ce91403c78935657
soir20 <71418127+soir20@users.noreply.github.com>
2021-01-16 16:51:42 -0500

    Store position: uploader does not need to know it

--- src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/NativeImageFrame.java
+++ src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/NativeImageFrame.java
@@ -7,19 +7,32 @@ import java.util.Set;
 
 public class NativeImageFrame extends AnimationFrame {
     private final MipmappedNativeImage IMAGE;
+    private final int X_POS;
+    private final int Y_POS;
+    private final int SKIP_X;
+    private final int SKIP_Y;
 
-    public NativeImageFrame(int frameIndexIn, MipmappedNativeImage image) {
+    public NativeImageFrame(int frameIndexIn, MipmappedNativeImage image,
+                            int xPos, int yPos, int skipX, int skipY) {
         super(frameIndexIn);
         IMAGE = image;
+        X_POS = xPos;
+        Y_POS = yPos;
+        SKIP_X = skipX;
+        SKIP_Y = skipY;
     }
 
-    public NativeImageFrame(int frameIndexIn, int frameTimeIn, MipmappedNativeImage image) {
+    public NativeImageFrame(int frameIndexIn, MipmappedNativeImage image,
+                            int xPos, int yPos, int skipX, int skipY, int frameTimeIn) {
         super(frameIndexIn, frameTimeIn);
         IMAGE = image;
+        X_POS = xPos;
+        Y_POS = yPos;
+        SKIP_X = skipX;
+        SKIP_Y = skipY;
     }
 
-    public void upload(int xPos, int yPos, int skipX, int skipY,
-                       boolean blur, boolean clamp, boolean autoClose) {
+    public void upload(boolean blur, boolean clamp, boolean autoClose) {
         Set<Integer> levels = IMAGE.getMipmapLevels();
         boolean shouldMipmap = levels.size() > 1;
 
@@ -31,10 +44,10 @@ public class NativeImageFrame extends AnimationFrame {
 
             // Uploading an empty image is a waste of time
             if (width > 0 && height > 0) {
-                int x = getMipmappedDimension(level, xPos);
-                int y = getMipmappedDimension(level, yPos);
-                int unpackSkipPixels = getMipmappedDimension(level, skipX);
-                int unpackSkipRows = getMipmappedDimension(level, skipY);
+                int x = getMipmappedDimension(level, X_POS);
+                int y = getMipmappedDimension(level, Y_POS);
+                int unpackSkipPixels = getMipmappedDimension(level, SKIP_X);
+                int unpackSkipRows = getMipmappedDimension(level, SKIP_Y);
 
                 IMAGE.getMipmap(level).uploadTextureSub(level, x, y, unpackSkipPixels, unpackSkipRows,
                         width, height, blur, clamp, shouldMipmap, autoClose);
