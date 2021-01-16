4082b22f5fb6f07b1014a135f1ebc057f35ea732
soir20 <71418127+soir20@users.noreply.github.com>
2021-01-15 23:51:00 -0500

    Comment NativeImage wrapper

--- src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/NativeImageRGBAWrapper.java
+++ src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/NativeImageRGBAWrapper.java
@@ -2,30 +2,60 @@ package io.github.soir20.moremcmeta.client.renderer.texture;
 
 import net.minecraft.client.renderer.texture.NativeImage;
 
+/**
+ * Wraps a {@link NativeImage} so it is compatible with the {@link IRGBAImage} interface and
+ * the {@link RGBAInterpolator}.
+ * @author soir20
+ */
 public class NativeImageRGBAWrapper implements IRGBAImage {
     private final NativeImage IMAGE;
 
+    /**
+     * Creates a new {@link NativeImage} wrapper.
+     * @param image     the image to wrap
+     */
     public NativeImageRGBAWrapper(NativeImage image) {
         IMAGE = image;
     }
 
+    /**
+     * Gets the color of a pixel in the image.
+     * @param x     x-coordinate of the pixel
+     * @param y     y-coordinate of the pixel
+     * @return  the color of the given pixel
+     */
     @Override
     public int getPixel(int x, int y) {
         return IMAGE.getPixelRGBA(x, y);
     }
 
+    /**
+     * Sets the color of a pixel in the image.
+     * @param x         x-coordinate of the pixel
+     * @param y         y-coordinate of the pixel
+     * @param color     new color of the pixel
+     */
     @Override
     public void setPixel(int x, int y, int color) {
         IMAGE.setPixelRGBA(x, y, color);
     }
 
+    /**
+     * Gets the width of the image.
+     * @return  the width of the image
+     */
     @Override
     public int getWidth() {
         return IMAGE.getWidth();
     }
 
+    /**
+     * Gets the height of the image.
+     * @return  the height of the image
+     */
     @Override
     public int getHeight() {
         return IMAGE.getHeight();
     }
+
 }
