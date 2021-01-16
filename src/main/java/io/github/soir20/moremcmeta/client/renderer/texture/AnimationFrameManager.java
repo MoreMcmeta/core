4b4b14dd1561aa0dc26a41714f6534b1201b1476
soir20 <71418127+soir20@users.noreply.github.com>
2021-01-16 15:01:31 -0500

    Use frame time function for more flexibility

--- src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/AnimationFrameManager.java
+++ src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/AnimationFrameManager.java
@@ -1,51 +1,55 @@
 package io.github.soir20.moremcmeta.client.renderer.texture;
 
 import net.minecraft.client.renderer.texture.ITickable;
-import net.minecraft.client.resources.data.AnimationFrame;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * Manages the current frame in an animation.
  * @param <T>   animation frame class
  * @author soir20
  */
-public class AnimationFrameManager<T extends AnimationFrame> implements ITickable {
+public class AnimationFrameManager<T> implements ITickable {
     private final List<T> INTERPOLATED_FRAMES;
-    private final int DEFAULT_FRAME_TIME;
+    private final Function<T, Integer> FRAME_TIME_CALCULATOR;
 
     private int ticksInThisFrame;
     private int currentFrame;
 
     /**
      * Creates an animation frame manager that does not interpolate between frames.
-     * @param frames            frames of the animation
-     * @param defaultFrameTime  default frame time if a frame has no time
+     * @param frames                frames of the animation
+     * @param frameTimeCalculator   calculates the frame time for a given frame.
+     *                              In most cases, pass a function that gets the
+     *                              time from the frame or returns a default value.
      */
-    public AnimationFrameManager(List<T> frames, int defaultFrameTime) {
-        this(defaultFrameTime);
+    public AnimationFrameManager(List<T> frames, Function<T, Integer> frameTimeCalculator) {
+        this(frameTimeCalculator);
 
         INTERPOLATED_FRAMES.addAll(frames);
     }
 
     /**
      * Creates an animation frame manager that interpolates between frames.
-     * @param frames            frames of the animation
-     * @param defaultFrameTime  default frame time if a frame has no time
-     * @param interpolator      interpolates between frames of the animation
+     * @param frames                frames of the animation
+     * @param frameTimeCalculator   calculates the frame time for a given frame.
+     *                              In most cases, pass a function that gets the
+     *                              time from the frame or returns a default value.
+     * @param interpolator          interpolates between frames of the animation
      */
-    public AnimationFrameManager(List<T> frames, int defaultFrameTime,
+    public AnimationFrameManager(List<T> frames, Function<T, Integer> frameTimeCalculator,
                                  BiFunction<T, T, Collection<T>> interpolator) {
-        this(defaultFrameTime);
+        this(frameTimeCalculator);
 
         int numFrames = frames.size();
         for (int frameIndex = 0; frameIndex < numFrames; frameIndex++) {
             T frame = frames.get(frameIndex);
 
-            if (getFrameTime(frame) > 1) {
+            if (FRAME_TIME_CALCULATOR.apply(frame) > 1) {
                 T nextFrame = frames.get((frameIndex + 1) % numFrames);
                 Collection<T> intermediateFrames = interpolator.apply(frame, nextFrame);
 
@@ -59,11 +63,13 @@ public class AnimationFrameManager<T extends AnimationFrame> implements ITickabl
 
     /**
      * Initializes common fields in a newly-created animation frame manager.
-     * @param defaultFrameTime  default frame time if a frame has no time
+     * @param frameTimeCalculator   calculates the frame time for a given frame.
+     *                              In most cases, pass a function that gets the
+     *                              time from the frame or returns a default value.
      */
-    private AnimationFrameManager(int defaultFrameTime) {
+    private AnimationFrameManager(Function<T, Integer> frameTimeCalculator) {
         INTERPOLATED_FRAMES = new ArrayList<>();
-        DEFAULT_FRAME_TIME = defaultFrameTime;
+        FRAME_TIME_CALCULATOR = frameTimeCalculator;
     }
 
     /**
@@ -81,19 +87,10 @@ public class AnimationFrameManager<T extends AnimationFrame> implements ITickabl
     public void tick() {
         ticksInThisFrame++;
 
-        if (ticksInThisFrame == getFrameTime(getCurrentFrame())) {
+        if (ticksInThisFrame == FRAME_TIME_CALCULATOR.apply(getCurrentFrame())) {
             currentFrame = (currentFrame + 1) % INTERPOLATED_FRAMES.size();
             ticksInThisFrame = 0;
         }
-    }
-
-    /**
-     * Gets a frame's length or the default time if it has none.
-     * @param frame     the frame to get the length of
-     * @return  the length of the current frame
-     */
-    private int getFrameTime(T frame) {
-        return frame.getFrameTime() == -1 ? DEFAULT_FRAME_TIME : frame.getFrameTime();
     }
 
 }
