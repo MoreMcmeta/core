8fbb63dd04d53181fea3c84201e0abef0c835849
soir20 <71418127+soir20@users.noreply.github.com>
2021-01-17 20:29:45 -0500

    Simplify frame based on new structures

--- src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/NativeImageFrame.java
+++ src/main/java/io/github/soir20/moremcmeta/client/renderer/texture/NativeImageFrame.java
@@ -1,66 +1,20 @@
 package io.github.soir20.moremcmeta.client.renderer.texture;
 
-import net.minecraft.client.renderer.texture.NativeImage;
-import net.minecraft.client.resources.data.AnimationFrame;
+public class NativeImageFrame {
+    private final RGBASubImage<NativeImageRGBAWrapper> IMAGE;
+    private final int FRAME_TIME;
 
-import java.util.Set;
-
-public class NativeImageFrame extends AnimationFrame {
-    private final MipmappedNativeImage IMAGE;
-    private final int X_POS;
-    private final int Y_POS;
-    private final int SKIP_X;
-    private final int SKIP_Y;
-
-    public NativeImageFrame(int frameIndexIn, MipmappedNativeImage image,
-                            int xPos, int yPos, int skipX, int skipY) {
-        super(frameIndexIn);
+    public NativeImageFrame(RGBASubImage<NativeImageRGBAWrapper> image, int frameTime) {
         IMAGE = image;
-        X_POS = xPos;
-        Y_POS = yPos;
-        SKIP_X = skipX;
-        SKIP_Y = skipY;
+        FRAME_TIME = frameTime;
     }
 
-    public NativeImageFrame(int frameIndexIn, MipmappedNativeImage image,
-                            int xPos, int yPos, int skipX, int skipY, int frameTimeIn) {
-        super(frameIndexIn, frameTimeIn);
-        IMAGE = image;
-        X_POS = xPos;
-        Y_POS = yPos;
-        SKIP_X = skipX;
-        SKIP_Y = skipY;
+    public void uploadAt(int x, int y) {
+        IMAGE.uploadAt(x, y);
     }
 
-    public void upload(boolean blur, boolean clamp, boolean autoClose) {
-        Set<Integer> levels = IMAGE.getMipmapLevels();
-        boolean shouldMipmap = levels.size() > 1;
-
-        for (int level : levels) {
-            NativeImage image = IMAGE.getMipmap(level);
-
-            int width = getMipmappedDimension(level, image.getWidth());
-            int height = getMipmappedDimension(level, image.getWidth());
-
-            // Uploading an empty image is a waste of time
-            if (width > 0 && height > 0) {
-                int x = getMipmappedDimension(level, X_POS);
-                int y = getMipmappedDimension(level, Y_POS);
-                int unpackSkipPixels = getMipmappedDimension(level, SKIP_X);
-                int unpackSkipRows = getMipmappedDimension(level, SKIP_Y);
-
-                IMAGE.getMipmap(level).uploadTextureSub(level, x, y, unpackSkipPixels, unpackSkipRows,
-                        width, height, blur, clamp, shouldMipmap, autoClose);
-            }
-
-        }
-    }
-
-    private int getMipmappedDimension(int level, int value) {
-
-        // Use bitwise shift to divide by a power of two to avoid rounding Math.pow(2, level)
-        return value >> level;
-
+    public int getFrameTime() {
+        return FRAME_TIME;
     }
 
 }
