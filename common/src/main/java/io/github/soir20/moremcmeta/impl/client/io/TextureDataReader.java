fa69d36aa4184e17bdd2366cdfb88d96b9c74087
soir20 <71418127+soir20@users.noreply.github.com>
2022-04-03 16:17:35 -0400

    Fix equality comparison and add comments

--- common/src/main/java/io/github/soir20/moremcmeta/impl/client/io/TextureDataReader.java
+++ common/src/main/java/io/github/soir20/moremcmeta/impl/client/io/TextureDataReader.java
@@ -51,6 +51,10 @@ import static java.util.Objects.requireNonNull;
 public class TextureDataReader implements TextureReader<TextureData<NativeImageAdapter>> {
     private final Map<String, MoreMcmetaPlugin> SECTION_TO_PLUGIN;
 
+    /**
+     * Creates a new reader that is aware of the given plugins, if any.
+     * @param plugins       plugins that the reader should use to parse texture data
+     */
     public TextureDataReader(Iterable<MoreMcmetaPlugin> plugins) {
         SECTION_TO_PLUGIN = new HashMap<>();
         plugins.forEach((plugin) -> SECTION_TO_PLUGIN.put(plugin.sectionName(), plugin));
@@ -108,6 +112,13 @@ public class TextureDataReader implements TextureReader<TextureData<NativeImageA
         );
     }
 
+    /**
+     * Reads JSON metadata from an input stream.
+     * @param metadataStream        metadata stream with JSON data
+     * @return a view of the parsed metadata. The keys are in order of how plugins
+     *         should be applied.
+     * @throws InvalidMetadataException if the input stream does not contain valid JSON
+     */
     private MetadataView readMetadata(InputStream metadataStream) throws InvalidMetadataException {
         BufferedReader bufferedReader = null;
         MetadataView metadata;
@@ -131,6 +142,22 @@ public class TextureDataReader implements TextureReader<TextureData<NativeImageA
         return metadata;
     }
 
+    /**
+     * Compares two section names at the topmost metadata level to
+     * determine plugin application order. Compares the sections
+     * based on priority and then based on lexicographical ordering
+     * of their names.
+     *
+     * If section1 should precede section2, a negative integer is returned.
+     * If section2 should precede section1, a positive integer is returned.
+     * If section1 and section2 are identical, then zero is returned.
+     * @param root          top-level view of the metadata
+     * @param section1      first section to compare
+     * @param section2      second section to compare
+     * @return a negative integer if section1 precedes section2, a positive
+     *         integer if section2 precedes section1, or zero if they are
+     *         the same
+     */
     private int compareSections(MetadataView root, String section1, String section2) {
         MetadataView view1 = root.subView(section1).orElseThrow();
         MetadataView view2 = root.subView(section2).orElseThrow();
@@ -145,10 +172,25 @@ public class TextureDataReader implements TextureReader<TextureData<NativeImageA
         return section1.compareTo(section2);
     }
 
+    /**
+     * Compares the current value and a possible new value to check if
+     * they are compatible. The two values are not compatible if and
+     * only if they are not both empty, and they contain different
+     * items according to {@link Object#equals(Object)}.
+     *
+     * If the two values are not compatible, an exception is thrown.
+     * Otherwise, the new value is returned.
+     * @param currentVal    current value
+     * @param newVal        new value that may be returned
+     * @param propName      property name describing what is contained in the values
+     * @param <T> type contained by the {@link Optional}s
+     * @return the new value if both values are compatible
+     * @throws InvalidMetadataException if the values are not compatible
+     */
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private <T> Optional<T> getIfCompatible(Optional<T> currentVal, Optional<T> newVal, String propName)
             throws InvalidMetadataException {
-        if (currentVal.isPresent() && newVal.isPresent() && currentVal.get() != newVal.get()) {
+        if (currentVal.isPresent() && newVal.isPresent() && !currentVal.get().equals(newVal.get())) {
             throw new InvalidMetadataException(String.format("%s was given conflicting values by two plugins: %s %s",
                     propName, currentVal.get(), newVal.get()));
         }
