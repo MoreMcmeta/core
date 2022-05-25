2cbdb0cd426a6aa10d907743c4bcee8500d1f19a
soir20 <71418127+soir20@users.noreply.github.com>
2022-05-25 01:50:02 -0400

    Add current frame view docs

--- common/src/main/java/io/github/soir20/moremcmeta/api/client/texture/CurrentFrameView.java
+++ common/src/main/java/io/github/soir20/moremcmeta/api/client/texture/CurrentFrameView.java
@@ -19,10 +19,32 @@ package io.github.soir20.moremcmeta.api.client.texture;
 
 import io.github.soir20.moremcmeta.api.math.Point;
 
+/**
+ * {@link FrameView} with the ability to adjust the current frame. Predefined frames are
+ * immutable, but a frame can be generated from a predefined frame or an already-generated
+ * frame.
+ * @author soir20
+ * @since 4.0
+ */
 public interface CurrentFrameView extends FrameView {
 
+    /**
+     * Generates a frame from the current frame by applying the given {@link ColorTransform} over
+     * the given area. The current frame may be a predefined frame or a generated frame. Predefined
+     * frames are never modified.
+     * @param transform     the transformation to apply to the given points
+     * @param applyArea     the points to apply the transformation to
+     * @throws io.github.soir20.moremcmeta.api.client.texture.FrameView.PixelOutOfBoundsException if a pixel
+     *         in the `applyArea` is out of the frame's bounds
+     */
     void generateWith(ColorTransform transform, Iterable<Point> applyArea);
 
+    /**
+     * Makes the predefined frame with the given index the current frame. The next call to
+     * {@link #generateWith(ColorTransform, Iterable)} by any plugin will generate a frame based
+     * on this predefined frame, unless another predefined frame is made the current frame.
+     * @param index     the index of the predefined frame to make current
+     */
     void replaceWith(int index);
 
 }
