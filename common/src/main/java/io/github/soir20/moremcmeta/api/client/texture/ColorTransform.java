8aeb47541a91fc294c1a9f05568a0b200f002d81
soir20 <71418127+soir20@users.noreply.github.com>
2022-05-25 00:27:49 -0400

    Add ColorTransform docs

--- common/src/main/java/io/github/soir20/moremcmeta/api/client/texture/ColorTransform.java
+++ common/src/main/java/io/github/soir20/moremcmeta/api/client/texture/ColorTransform.java
@@ -17,9 +17,24 @@
 
 package io.github.soir20.moremcmeta.api.client.texture;
 
+/**
+ * Calculates a new RGBA color for an individual pixel in a frame.
+ * @author soir20
+ * @since 4.0
+ */
 @FunctionalInterface
 public interface ColorTransform {
 
+    /**
+     * Calculates the new color of the pixel at the given coordinate in the frame being modified.
+     * While this method only takes the x and y coordinates of the pixel, a {@link FrameView} for
+     * the frame being modified is available in contexts where a {@link ColorTransform} would be
+     * provided.
+     * @param x     x-coordinate of the pixel whose color will be replaced
+     * @param y     y-coordinate of the pixel whose color will be replaced
+     * @return the new color of the pixel at (x, y) in the format
+     *         AAAA AAAA RRRR RRRR GGGG GGGG BBBB BBBB (32 bits)
+     */
     int transform(int x, int y);
 
 }
