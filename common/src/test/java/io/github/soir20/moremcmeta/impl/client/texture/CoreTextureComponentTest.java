049b9718604f6fd4a91a98a7ac2a892aea98819c
soir20 <71418127+soir20@users.noreply.github.com>
2022-05-31 16:33:31 -0400

    Add CoreTextureComponent tests

---
+++ common/src/test/java/io/github/soir20/moremcmeta/impl/client/texture/CoreTextureComponentTest.java
@@ -0,0 +1,102 @@
+/*
+ * MoreMcmeta is a Minecraft mod expanding texture animation capabilities.
+ * Copyright (C) 2022 soir20
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.soir20.moremcmeta.impl.client.texture;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Tests the default methods of the {@link CoreTextureComponent}.
+ * @author soir20
+ */
+public class CoreTextureComponentTest {
+
+    @Test
+    public void onRegistration_NullState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+        component.onRegistration(null);
+    }
+
+    @Test
+    public void onUpload_NullState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+        component.onUpload(null);
+    }
+
+    @Test
+    public void onTick_NullState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+        component.onTick(null);
+    }
+
+    @Test
+    public void onClose_NullState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+        component.onClose(null);
+    }
+
+    @Test
+    public void onRegistration_ValidState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+
+        EventDrivenTexture.Builder builder = new EventDrivenTexture.Builder();
+        builder.setGeneratedFrame(new MockCloseableImageFrame());
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame()));
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame(), new MockCloseableImageFrame()));
+        builder.add(component);
+        builder.build().load(null);
+    }
+
+    @Test
+    public void onUpload_ValidState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+
+        EventDrivenTexture.Builder builder = new EventDrivenTexture.Builder();
+        builder.setGeneratedFrame(new MockCloseableImageFrame());
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame()));
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame(), new MockCloseableImageFrame()));
+        builder.add(component);
+        builder.build().upload();
+    }
+
+    @Test
+    public void onTick_ValidState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+
+        EventDrivenTexture.Builder builder = new EventDrivenTexture.Builder();
+        builder.setGeneratedFrame(new MockCloseableImageFrame());
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame()));
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame(), new MockCloseableImageFrame()));
+        builder.add(component);
+        builder.build().tick();
+    }
+
+    @Test
+    public void onClose_ValidState_NoException() {
+        CoreTextureComponent component = new CoreTextureComponent() {};
+
+        EventDrivenTexture.Builder builder = new EventDrivenTexture.Builder();
+        builder.setGeneratedFrame(new MockCloseableImageFrame());
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame()));
+        builder.setPredefinedFrames(List.of(new MockCloseableImageFrame(), new MockCloseableImageFrame()));
+        builder.add(component);
+        builder.build().close();
+    }
+
+}
