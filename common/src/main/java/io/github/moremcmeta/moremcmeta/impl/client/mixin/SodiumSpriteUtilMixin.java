d5d4f58ab7e27eccf747a8ae378da2d977ffe1da
soir20 <71418127+soir20@users.noreply.github.com>
2024-11-11 21:32:02 -0500

    Disable remapping on Sodium mixin

--- common/src/main/java/io/github/moremcmeta/moremcmeta/impl/client/mixin/SodiumSpriteUtilMixin.java
+++ common/src/main/java/io/github/moremcmeta/moremcmeta/impl/client/mixin/SodiumSpriteUtilMixin.java
@@ -32,8 +32,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  * @author soir20
  */
 @SuppressWarnings("unused")
-@Mixin(value = {me.jellysquid.mods.sodium.client.render.texture.SpriteUtil.class},
-        remap = false)
+@Mixin(value = {me.jellysquid.mods.sodium.client.render.texture.SpriteUtil.class})
 public class SodiumSpriteUtilMixin {
 
     /**
